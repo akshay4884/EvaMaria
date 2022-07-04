7d8836927bb53329f9b1992a35e23fe182072032
akshay4884 <89313089+akshay4884@users.noreply.github.com>
2022-07-04 20:48:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -759,8 +759,7 @@ async def auto_filter(client, msg, spoll=False):
 
     if spoll:
 
-        await msg.message.delete()
-        
+        await msg.message.delete() 
         
         async def advantage_spell_chok(msg):
     query = re.sub(
