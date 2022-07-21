8fb540e49242af2a6ac7b3af843947ab8475e941
akshay4884 <89313089+akshay4884@users.noreply.github.com>
2022-07-21 16:49:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ async def give_filter(client, message):
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("{message.from_user.mention} മോനു അത് നിനക്കുള്ളതല്ല😊\n\n𝐒𝐞𝐚𝐫𝐜𝐡 𝐲𝐨𝐮𝐫 𝐨𝐰𝐧 𝐟𝐢𝐥𝐞𝐬, 𝐝𝐨𝐧'𝐭 𝐜𝐥𝐢𝐜𝐤 𝐨𝐧 𝐨𝐭𝐡𝐞𝐫𝐬 𝐫𝐞𝐬𝐮𝐥𝐭𝐬 👀", show_alert=True)
     try:
         offset = int(offset)
     except:
@@ -120,7 +120,9 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("okDa", show_alert=True)
+        return await query.answer("{message.from_user.mention} മോനു അത് നിനക്കുള്ളതല്ല😊\n\n𝐒𝐞𝐚𝐫𝐜𝐡 𝐲𝐨𝐮𝐫 𝐨𝐰𝐧 𝐟𝐢𝐥𝐞𝐬, 𝐝𝐨𝐧'𝐭 𝐜𝐥𝐢𝐜𝐤 𝐨𝐧 𝐨𝐭𝐡𝐞𝐫𝐬 𝐫𝐞𝐬𝐮𝐥𝐭𝐬 👀")
+
+", show_alert=True)
     if movie_ == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
